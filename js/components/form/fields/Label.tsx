d74d239b70310e97be840f3b1cdc91edaaf18bdb
Singletonn <sopranowest@gmail.com>
2022-04-03 15:36:44 +1000

    Для лейбла добавил возможность указать класс

--- js/components/form/fields/Label.tsx
+++ js/components/form/fields/Label.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
 
 type PropsType = React.LabelHTMLAttributes<any> & {
+  className: string;
   label: string | number,
   isRequired?: boolean,
 }
+
+const defaultProps = {
+  className: 'form-label',
+};
 
 const Label: React.VFC<PropsType> = ({
   label,
   isRequired,
   htmlFor,
+  className
 }) => {
   if (!label) {
     return null;
   }
 
   return (
-    <label className="form-label" htmlFor={htmlFor}>
+    <label className={className} htmlFor={htmlFor}>
       {label}
       {isRequired ? <span className="text-danger"> *</span> : null}
     </label>
   );
 };
+
+Label.defaultProps = defaultProps;
 
 export { Label };

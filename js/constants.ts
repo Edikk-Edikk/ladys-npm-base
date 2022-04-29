d25732799cba874926c09da0579cf26db2119e6a
Singletonn <sopranowest@gmail.com>
2022-04-29 18:24:32 +1000

    Изменил название констант

--- js/constants.ts
+++ js/constants.ts
@@ -1,13 +1,13 @@
 const ERROR_MESSAGE_DEFAULT = 'Error, our specialists already know about it and take measures';
 const COOKIE_AUTH_TOKEN = 'authToken';
-const COOKIE_TOKEN = 'token';
+const COOKIE_REQUEST_ID = 'token';
 const HEADER_X_API_AUTH_TOKEN = 'X-Api-Auth-Token';
-const HEADER_X_API_TOKEN = 'X-Api-Token';
+const HEADER_X_API_REQUEST_ID = 'X-Api-Request-ID';
 
 export {
   ERROR_MESSAGE_DEFAULT,
   COOKIE_AUTH_TOKEN,
-  COOKIE_TOKEN,
+  COOKIE_REQUEST_ID,
   HEADER_X_API_AUTH_TOKEN,
-  HEADER_X_API_TOKEN,
+  HEADER_X_API_REQUEST_ID,
 };

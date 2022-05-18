f587b53782b8c193d8b39b8234cc0b4465527c06
singletonn <sopranowest@gmail.com>
2022-05-18 17:51:48 +1000

    Доработал экспорт компонента списка стоисов

--- js/components/stories/index.ts
+++ js/components/stories/index.ts
@@ -2,5 +2,6 @@ export * from './detail';
 export * from './detail-list';
 export * from './management';
 export * from './stories';
+export * from './stories-list';
 export * from './swiper';
 export * from './types';

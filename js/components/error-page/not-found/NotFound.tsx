2e4e2edb0d9736891cc5d244dffa45f313fa94e9
singletonn <sopranowest@gmail.com>
2022-06-04 20:30:03 +1000

    Для страницы ошибок ушли от css module

--- js/components/error-page/not-found/NotFound.tsx
+++ js/components/error-page/not-found/NotFound.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import errorPageCss from '../assets/error-page.module.scss';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
-import useStyles from 'isomorphic-style-loader/useStyles';
 
 const NotFound = () => {
-  useStyles(errorPageCss);
   return (
-    <div className={errorPageCss.errorPage}>
-      <div className={errorPageCss.errorPage__title}>
+    <div className="error-page">
+      <div className="error-page__title">
         4😵4
       </div>
-      <div className={errorPageCss.errorPage__description}>
+      <div className="error-page__action">
         The page you were looking for was not found.
       </div>
-      <Link to="/" className={classNames(errorPageCss.errorPage__action, 'btn btn-primary btn-lg')}>
+      <Link to="/" className={classNames("error-page__action", 'btn btn-primary btn-lg')}>
         Click here to go home
       </Link>
     </div>
